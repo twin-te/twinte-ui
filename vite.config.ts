956c945e1a61b2db8fdc5ae2a9269f7013bcf149
hayato24s <engr.hayato24s@gmail.com>
2023-02-18 10:56:56 +0900

    Add vite in library mode

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import dts from "vite-plugin-dts";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      // 複数のエントリーポイントのディクショナリや配列にもできます
+      entry: "src/main.ts",
+      // 適切な拡張子が追加されます
+      fileName: "twinte-ui",
+      formats: ["es"],
+    },
+    rollupOptions: {
+      // ライブラリにバンドルされるべきではない依存関係を
+      // 外部化するようにします
+      external: ["vue"],
+    },
+  },
+  plugins: [vue(), dts()],
+});
